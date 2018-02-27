4d575c3024e7e68a3701171ed4d721b0da9fe8c5
ql15955614482 <925697386@qq.com>
2018-02-27 17:56:28 +0800

    2.27.3

--- src/store/modules/account.js
+++ src/store/modules/account.js
@@ -1,19 +1,9 @@
 const account = {
   state: {
-    token: localStorage.token == null ? [] : JSON.parse(localStorage.token)
   },
   mutations: {
-    // token
-    SET_TOKEN: (state, token) => {
-      state.token = token
-      localStorage['token'] = JSON.stringify(token) // 由于 localStorage 只能保存字符串内容，所以这里要先把对象转换成 JSON 字符串
-    }
   },
   actions: {
-    // token
-    changeToken: ({ commit }, token) => {
-      commit('SET_TOKEN', token)
-    }
   }
 }
 
