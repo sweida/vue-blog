5790c3916ae438c2f7854a5fdf7568c901643904
ql15955614482 <925697386@qq.com>
2018-03-27 21:20:29 +0800

    3.27

--- src/view/clubCard/clubCard.vue
+++ src/view/clubCard/clubCard.vue
@@ -1,157 +1,113 @@
 <template>
-  <div >
-      <div class="header_title">会员卡<i class="el-icon-info"></i></div>
-      <div class="main-content scroll">
-        <div class="left_box">
-          <h4>会员卡信息</h4>
-          <div class="club_box">
-            <div class="input_box">
-              <li>
-                <label for="">会员卡级别</label>
-                <el-input size="small" placeholder="请输入会员级别" v-model="vipinput.schemeName"></el-input>
-              </li>
-              <li>
-                <label for="">起点金额</label>
-                <el-input size="small" v-model="vipinput.schemePrice"></el-input>
-              </li>
-              <li>
-                <label for="">有效期（月）</label>
-                <el-input size="small" v-model="vipinput.vipValidityDate"></el-input>
-              </li>
-              <li>
-                <label for="">项目折扣</label>
-                <el-input size="small" v-model="vipinput.projectDiscount"></el-input>
-              </li>
-              <li>
-                <label for="">产品折扣</label>
-                <el-input size="small" v-model="vipinput.productDiscount"></el-input>
-              </li>
-              <li>
-                <label for="">套餐折扣</label>
-                <el-input size="small" v-model="vipinput.packageDiscount"></el-input>
-              </li>
-            </div>
-            <div class="addpresent">
-              <p @click="addpresent">添加赠送+</p>
-              <div>
-                <el-tag
-                  v-for="(item,index) in materials_data"
-                  :key="materials_data.givePlanName"
-                  closable
-                  :disable-transitions="false"
-                  @close="handleTagsClose(item)"
-                  >
-                  {{(item.projectName)+'(*'+item.projectNum+')'}}
-                </el-tag>
-              </div>
-            </div>
+<div>
+  <div class="header_title">会员卡<i class="el-icon-info"></i></div>
+  <div class="main-content scroll">
+    <div class="left_box">
+      <h4>会员卡信息</h4>
+      <div class="club_box">
+        <div class="input_box">
+          <li>
+            <label for="">会员卡级别</label>
+            <el-input size="small" placeholder="请输入会员级别" v-model="vipinput.schemeName"></el-input>
+          </li>
+          <li>
+            <label for="">起点金额</label>
+            <el-input size="small" v-model="vipinput.schemePrice"></el-input>
+          </li>
+          <li>
+            <label for="">有效期（月）</label>
+            <el-input size="small" v-model="vipinput.vipValidityDate"></el-input>
+          </li>
+          <li>
+            <label for="">项目折扣</label>
+            <el-input size="small" v-model="vipinput.projectDiscount"></el-input>
+          </li>
+          <li>
+            <label for="">产品折扣</label>
+            <el-input size="small" v-model="vipinput.productDiscount"></el-input>
+          </li>
+          <li>
+            <label for="">套餐折扣</label>
+            <el-input size="small" v-model="vipinput.packageDiscount"></el-input>
+          </li>
+        </div>
+        <div class="addpresent">
+          <p @click="addpresent">添加赠送+</p>
+          <div>
+            <el-tag v-for="(item,index) in materials_data" :key="materials_data.givePlanName" closable :disable-transitions="false" @close="handleTagsClose(item)">
+              {{(item.projectName)+'(*'+item.projectNum+')'}}
+            </el-tag>
           </div>
-          <el-button type="primary" size="small" @click="vipinputBtn" v-if="change">确认新增</el-button>
-          <template v-else>
+        </div>
+      </div>
+      <el-button type="primary" size="small" @click="vipinputBtn" v-if="change">确认新增</el-button>
+      <template v-else>
             <el-button type="primary" size="small" @click="sureChange">保　存</el-button>
             <el-button  size="small" @click="cancelSave">取　消</el-button>
           </template>
-          <el-table
-            :data="vipcardList"
-            stripe
-            style="width: 100%"
-            max-height="345"
-            tooltip-effect="dark"
-            >
-            <el-table-column
-              prop="schemeName"
-              label="会员卡级别"
-              sortable>
-            </el-table-column>
-            <el-table-column
-              prop="schemePrice"
-              label="起点金额"
-              sortable>
-            </el-table-column>
-            <el-table-column
-              prop="productDiscount"
-              label="项目折扣"
-              >
-            </el-table-column>
-            <el-table-column
-              prop="projectDiscount"
-              label="产品折扣"
-              >
-            </el-table-column>
-            <el-table-column
-              prop="packageDiscount"
-              label="套餐折扣"
-              >
-            </el-table-column>
-            <el-table-column
-              label="修改">
-              <template slot-scope="scope">
+      <el-table :data="vipcardList" stripe style="width: 100%" max-height="345" tooltip-effect="dark">
+        <el-table-column prop="schemeName" label="会员卡级别" sortable>
+        </el-table-column>
+        <el-table-column prop="schemePrice" label="起点金额" sortable>
+        </el-table-column>
+        <el-table-column prop="productDiscount" label="项目折扣">
+        </el-table-column>
+        <el-table-column prop="projectDiscount" label="产品折扣">
+        </el-table-column>
+        <el-table-column prop="packageDiscount" label="套餐折扣">
+        </el-table-column>
+        <el-table-column label="修改">
+          <template slot-scope="scope">
                 <i class="el-icon-edit" @click="editvipBtn(scope.$index, scope.row)"></i>
               </template>
-            </el-table-column>
-            <el-table-column
-              label="删除">
-              <template slot-scope="scope">
+        </el-table-column>
+        <el-table-column label="删除">
+          <template slot-scope="scope">
                 <i class="el-icon-delete" @click="deleteVip(scope.$index, scope.row)"></i>
               </template>
-            </el-table-column>
-          </el-table>
-          <page :pageModel="pageModel" @selectList="selectRoleList" v-if="pageModel.sumCount>10"></page>
-        </div>
-        <div class="explain">
-          <h4>活动说明</h4>
-          <textarea name="" id="" cols="30" rows="10" class="scroll" v-model="actDesc"></textarea>
-          <el-button type="primary" size="small" @click="subText">提　交</el-button>
-        </div>
-      </div>
+        </el-table-column>
+      </el-table>
+      <page :pageModel="pageModel" @selectList="selectRoleList" v-if="pageModel.sumCount>10"></page>
+    </div>
+    <div class="explain">
+      <h4>活动说明</h4>
+      <textarea name="" id="" cols="30" rows="10" class="scroll" v-model="actDesc"></textarea>
+      <el-button type="primary" size="small" @click="subText">提　交</el-button>
+    </div>
+  </div>
 
-      <el-dialog :visible.sync="burdening" title="添加赠送" width="1050px" class="burbox">
-        <div class="tableDialog">
-          <div class="tabs">
-            <p class="nav-title">
-              <span>{{menuList.name}}</span>
-            </p>
-            <el-menu
-              class="el-menu-vertical-demo"
-              @open="handleOpen"
-              @close="handleClose"
-              @select="handleSelect">
-              <template v-for="(item, index) in menuList.childMenu" :keys="index">
+  <el-dialog :visible.sync="burdening" title="添加赠送" width="1050px" class="burbox">
+    <div class="tableDialog">
+      <div class="tabs">
+        <p class="nav-title">
+          <span>{{menuList.name}}</span>
+        </p>
+        <el-menu class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" @select="handleSelect">
+          <template v-for="(item, index) in menuList.childMenu" :keys="index">
                 <el-menu-item :index="item.url" v-if="item.childMenu==null || item.childMenu==''" @click="changeMenu(item)">
                   <template slot="title">
                     <span>{{item.name}}</span>
                   </template>
-                </el-menu-item>
-                <el-submenu :index="item.url"  v-else>
-                  <template slot="title" >
+          </el-menu-item>
+          <el-submenu :index="item.url" v-else>
+            <template slot="title">
                     <div @click="changeMenu(item)">
                       <span >{{item.name}}</span>
                     </div>
                   </template>
-                  <template v-for="(child, index1) in item.childMenu" :keys="index1">
+            <template v-for="(child, index1) in item.childMenu" :keys="index1">
                     <el-menu-item :index="child.url" @click="changeMenu(child)">
                       <span>{{child.name}}</span>
                     </el-menu-item>
                   </template>
-                </el-submenu>
-              </template>
-            </el-menu>
-          </div>
-          <div class="burli1">
-            <el-table
-              ref="goods"
-              :data="materials_arr"
-              stripe
-              style="width:100%"
-              max-height='450'
-              tooltip-effect="dark"
-              @select="selectGoods"
-              @select-all="selectAllGoods"
-              @selection-change="addtableList"
-              @expand-change="getSetMealDetails"
-              >
-              <el-table-column type="expand">
-                <template slot-scope="scope" v-loading="loading">
+          </el-submenu>
+          </template>
+        </el-menu>
+      </div>
+      <div class="burli1">
+        <el-table ref="goods" :data="materials_arr" stripe style="width:100%" max-height='450' tooltip-effect="dark" @select="selectGoods" @select-all="selectAllGoods" @selection-change="addtableList" @expand-change="getSetMealDetails">
+          <el-table-column type="expand">
+            <template slot-scope="scope" v-loading="loading">
                   <div  v-if="!loading">
                     <div class="item-detail">
                       <li>类型</li>
@@ -167,34 +123,20 @@
                     </div>
                   </div>
                 </template>
-              </el-table-column>
-              <el-table-column
-                prop="projectName"
-                label="名称"
-                >
-              </el-table-column>
-              <el-table-column
-                prop="effectiveDays"
-                label="有效天数">
-              </el-table-column>
-              <el-table-column
-              type="selection"
-              label="选择"
-              width="80">
-              </el-table-column>
-            </el-table>
-          </div>
-          <!-- 默认赠送 -->
-          <div class="burli2">
-            <el-table
-              :data="materials_data"
-              style="width: 100%;"
-              max-height='450'
-              tooltip-effect="dark"
-              @expand-change="getSetMealDetails"
-            >
-              <el-table-column type="expand">
-                <template slot-scope="scope" v-loading="loading">
+          </el-table-column>
+          <el-table-column prop="projectName" label="名称">
+          </el-table-column>
+          <el-table-column prop="effectiveDays" label="有效天数">
+          </el-table-column>
+          <el-table-column type="selection" label="选择" width="80">
+          </el-table-column>
+        </el-table>
+      </div>
+      <!-- 默认赠送 -->
+      <div class="burli2">
+        <el-table :data="materials_data" style="width: 100%;" max-height='450' tooltip-effect="dark" @expand-change="getSetMealDetails">
+          <el-table-column type="expand">
+            <template slot-scope="scope" v-loading="loading">
                   <div  v-if="!loading">
                     <div class="item-detail">
                       <li>类型</li>
@@ -210,33 +152,28 @@
                     </div>
                   </div>
                 </template>
-              </el-table-column>
-              <el-table-column
-                label="名称">
-                <template slot-scope="scope">
+          </el-table-column>
+          <el-table-column label="名称">
+            <template slot-scope="scope">
                   {{scope.row.projectName||scope.row.giftName}}
                 </template>
-              </el-table-column>
-              <el-table-column
-                prop="effectiveDays"
-                label="有效天数">
-              </el-table-column>
-              <el-table-column
-                prop="projectNum"
-                label="数量">
-                <template slot-scope="scope" >
+          </el-table-column>
+          <el-table-column prop="effectiveDays" label="有效天数">
+          </el-table-column>
+          <el-table-column prop="projectNum" label="数量">
+            <template slot-scope="scope">
                   <el-input-number v-model="scope.row.projectNum"  :min='1'></el-input-number>
                 </template>
-              </el-table-column>
-            </el-table>
-          </div>
-        </div>
-        <span slot="footer" class="dialog-footer">
+          </el-table-column>
+        </el-table>
+      </div>
+    </div>
+    <span slot="footer" class="dialog-footer">
           <el-button type="primary" @click="sureBurden" size="small">确 定</el-button>
         </span>
-      </el-dialog>
+  </el-dialog>
 
-  </div>
+</div>
 </template>
 
 <script>
@@ -324,10 +261,6 @@ export default {
         })
       }
     },
-    // 删除已选
-    delSelect(index) {
-      this.materials_data.splice(index, 1)
-    },
     subText() {
       let param = {
         actDesc: this.actDesc,
@@ -342,7 +275,12 @@ export default {
       })
     },
     handleTagsClose(tag) {
+      console.log(tag, this.materials_data, this.checkGoodIds)
       this.materials_data.splice(this.materials_data.indexOf(tag), 1)
+      this.checkGoodIds.splice(this.materials_data.indexOf(tag), 1)
+      this.materials_arr.splice(this.materials_arr.findIndex(item => {
+        return item.id == tag.id
+      }), 1)
     },
     // 弹框
     addpresent() {
@@ -410,14 +348,14 @@ export default {
             this.$message.error('删除失败!')
           }
         })
-      }).catch(() => {
-      })
+      }).catch(() => {})
     },
 
     // 编辑会员卡 id
     editvipBtn(index, row) {
       this.change = false
       this.id = row.id
+      this.materials_arr = []
       getCardDetail(row.id).then(res => {
         console.log(res, res.data.data.ccSelectedProjectVoList)
         this.vipinput = res.data.data
@@ -509,106 +447,104 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.main-content{
-  display: flex;
-  justify-content: space-between;
-  height: calc(100% - 50px);
+.main-content {
+    display: flex;
+    justify-content: space-between;
+    height: calc(100% - 50px);
 }
-.left_box{
-  flex: 1;
-  padding-right: 40px;
-  border-right: 1px solid #ddd;
-  .club_box{
-    width: 100%;
-    border: 1px solid #C0CCDA;
-    .input_box{
-      padding-top: 30px;
-      display: flex;
-      flex-wrap: wrap;
-      border-bottom: 1px solid #EBEBEB;
-      li{
-        display: flex;
-        align-items: center;
-        width: 300px;
-        padding-bottom:30px;
-        label{
-          width: 100px;
-          padding-right: 10px;
-          text-align: right;
+.left_box {
+    flex: 1;
+    padding-right: 40px;
+    border-right: 1px solid #ddd;
+    .club_box {
+        width: 100%;
+        border: 1px solid #C0CCDA;
+        .input_box {
+            padding-top: 30px;
+            display: flex;
+            flex-wrap: wrap;
+            border-bottom: 1px solid #EBEBEB;
+            li {
+                display: flex;
+                align-items: center;
+                width: 300px;
+                padding-bottom: 30px;
+                label {
+                    width: 100px;
+                    padding-right: 10px;
+                    text-align: right;
+                }
+                .el-input {
+                    width: 140px;
+                }
+            }
         }
-        .el-input{
-          width: 140px;
+        .addpresent {
+            display: flex;
+            align-items: center;
+            margin: 7px 0;
+            p {
+                color: #3BB3FF;
+                width: 120px;
+                text-align: center;
+                border-right: 1px solid #ddd;
+                line-height: 32px;
+                margin: 5px 0;
+                cursor: pointer;
+            }
+            .el-tag {
+                margin: 5px 0 5px 20px;
+            }
         }
-      }
     }
-    .addpresent{
-      display: flex;
-      align-items: center;
-      margin:7px 0;
-      p{
-        color: #3BB3FF;
-        width: 120px;
-        text-align: center;
-        border-right: 1px solid #ddd;
-        line-height: 32px;
-        margin: 5px 0;
-        cursor: pointer;
-      }
-      .el-tag{
-        margin: 5px 0 5px 20px;
-      }
+    .el-button {
+        margin: 20px 0 30px 20px;
+        float: right;
     }
-  }
-  .el-button{
-    margin: 20px 0 30px 20px;
-    float: right;
-  }
 }
-.explain{
-  width: 400px;
-  color:#475669;
-  padding: 0;
-  padding-left: 40px;
-  box-sizing: border-box;
-  textarea{
-    padding:10px;
-    line-height: 26px;
-    font-size: 14px;
-    resize: none;
-    border: 0;
-    background: #f7f7f7;
-    color: #3a4354;
-    width: 100%;
-    height: 200px;
-    outline: none;
-    margin-bottom: 20px;
+.explain {
+    width: 400px;
+    color: #475669;
+    padding: 0 0 0 40px;
     box-sizing: border-box;
-  }
-  .el-button{
-    float: right;
-  }
+    textarea {
+        padding: 10px;
+        line-height: 26px;
+        font-size: 14px;
+        resize: none;
+        border: 0;
+        background: #f7f7f7;
+        color: #3a4354;
+        width: 100%;
+        height: 200px;
+        outline: none;
+        margin-bottom: 20px;
+        box-sizing: border-box;
+    }
+    .el-button {
+        float: right;
+    }
 }
-.el-table{
-  margin-bottom: 25px;
+.el-table {
+    margin-bottom: 25px;
 }
-.item-detail{
-  width: 100%;
-  line-height: 32px;
-  text-align: center;
-  display: flex;
-  justify-content: space-around;
-  li{
-    flex:1;
+.item-detail {
+    width: 100%;
+    line-height: 32px;
     text-align: center;
-  }
-  li:nth-of-type(2){
-    flex:2;
-  }
+    display: flex;
+    justify-content: space-around;
+    li {
+        flex: 1;
+        text-align: center;
+    }
+    li:nth-of-type(2) {
+        flex: 2;
+    }
 }
-.item-detail:first-child{
-  line-height: 36px;
-  background: #f4f4f4;
-  font-weight:bold;
+.item-detail:first-child {
+    line-height: 36px;
+    background: #f4f4f4;
+    font-weight: bold;
 }
-
 </style>
