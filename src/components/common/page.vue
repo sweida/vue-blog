a479e6f62d323227d55103dbefd9e50d83321ab1
ql15955614482 <925697386@qq.com>
2018-05-08 10:21:26 +0800

    5.8.2

---
+++ src/components/common/page.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="page">
+    <el-pagination class="page"
+                   @size-change="handleSizeChange"
+                   @current-change="pageChange"
+                   :current-page="pageModel.page"
+                   :page-sizes="[5,10,15,20]"
+                   :page-size="pageModel.rows"
+                   background
+                   small
+                   layout="total, sizes, prev, pager, next, jumper"
+                   :total="pageModel.sumCount">
+    </el-pagination>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'page',
+    props: {pageModel: {}},
+    methods: {
+      //每页条数变化事件dd
+      handleSizeChange(val) {
+        this.pageModel.rows = val
+        this.$emit("selectList");
+      },
+      //页数变化事件
+      pageChange(val) {
+        this.pageModel.page = val
+        this.$emit("selectList")
+      }
+    }
+  }
+</script>
+<style>
+  #page {
+    text-align: center;
+    margin-top: 20px;
+  }
+</style>
